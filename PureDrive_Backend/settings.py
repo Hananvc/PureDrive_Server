c5b633c63ff34a844a5136058109637c9a6c732b
Hanan <hanan.vc@gmail.com>
2023-09-08 09:47:04 +0530

    env updated

--- PureDrive_Backend/settings.py
+++ PureDrive_Backend/settings.py
@@ -77,8 +77,8 @@ MIDDLEWARE = [
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUD_NAME'),
-    'API_KEY': env('API_KEY'),
-    'API_SECRET': env('API_SECRET'),
+    'API_KEY': env('CLOUDINARY_API_KEY'),
+    'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
 
 ROOT_URLCONF = 'PureDrive_Backend.urls'
